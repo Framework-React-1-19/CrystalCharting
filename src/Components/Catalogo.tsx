b232ec82076fb3a4d2ab83f238c9e1609d063d43
Ken Tamares <ken.tamares.15-1@accademiadigitaleliguria.it>
2026-07-21 15:31:59 +0200

    Update pagina Home

--- src/Components/Catalogo.tsx
+++ src/Components/Catalogo.tsx
@@ -1,12 +1,53 @@
-import { useState } from "react";
-import { Outlet, useNavigate, Link } from "react-router-dom";
-import { Box, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import {
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  Button,
+  Chip,
+  Stack,
+  Box,
+} from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
+import DirectionsBoatIcon from "@mui/icons-material/DirectionsBoat";
+import { Link } from "react-router-dom";
+
+interface Barca {
+  id: number;
+  nomebarca: string;
+  tipo: string;
+  alimentazione: string;
+  capienza: number;
+  cabine: number;
+  lunghezza: string;
+  potenza: string;
+  descrizione: string;
+  costo: number;
+}
 
 export function Catalogo() {
+  const [imbarcazioni, setImbarcazioni] = useState<Barca[]>([]);
+
+  useEffect(() => {
+    fetch("https://crystalcharting.awardspace.net/api.php?action=get_barche")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  });
+
   return (
     <Box>
-      <Typography>Catalogo principale</Typography>
-      {/* Qui verrà creata la griglia con le card delle imbarcazioni */}
+      <Typography
+        variant="h4"
+        component="h1"
+        gutterBottom
+        sx={{ fontWeight: "bold", mb: 3 }}
+      >
+        La Nostra Flotta
+      </Typography>
+      <Grid container spacing={2}></Grid>
     </Box>
   );
 }
